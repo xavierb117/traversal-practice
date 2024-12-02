5230ccc48e6696016d846c39fe01a758baf0ca15
Auberon Lopez <auberon.lopez@greenriver.edu>
2024-12-02 09:04:09 -0800

    Reorder and clarify

--- TraversalPractice.java
+++ TraversalPractice.java
@@ -14,22 +14,25 @@ public class TraversalPractice {
   /**
    * Prints the values of the nodes that have exactly one child in a tree.
    * Each value is printed on a separate line.
-   * The nodes are traversed pre-order
+   * The nodes are traversed pre-order.
    * 
    * @param <T> The type of value the nodes hold
    * @param node The root of the tree to print
    */
   private static <T> void printNodesWithOneChild(Node<T> node) {
-
+    
   }
 
   /**
-   * Returns the sum of the values of all nodes in a tree.
-   *  
+   * Returns the maximum value stored in a tree.
+   * Assumes all values are positive.
+   * Edge case:
+   *   Returns 0 if node is null.
+   * 
    * @param node The root of the tree
-   * @return the sum 
+   * @return the max value
    */
-  private static int treeSum(Node<Integer> node) {
+  private static int maxVal(Node<Integer> node) {
     return 0;
   }
 
@@ -41,20 +44,70 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   private static void printOddNodes(Node<Integer> node) {
-    
+
+  }
+
+    /**
+   * Returns the sum of the values of all nodes in a tree.
+   * Edge Case:
+   *   Returns 0 if node is null
+   *  
+   * @param node The root of the tree
+   * @return the sum 
+   */
+  private static int treeSum(Node<Integer> node) {
+    return 0;
   }
 
   /**
-   * Returns the maximum value stored in a tree.
+   * Returns the number of levels in the tree.
+   * Edge cases: 
+   *   Returns 0 if node is null, returns 1 if there is only a root with no children.
    * 
+   * @param <T> The type of value the nodes hold
    * @param node The root of the tree
-   * @return the max value
+   * @return The number of levels in the tree
    */
-  private static int maxVal(Node<Integer> node) {
+  private static <T> int numLevels(Node<T> node) {
     return 0;
   }
 
   public static void main(String[] args) {
+    /*
+     *         99
+     *       /    \
+     *     45      82
+     *    /  \        \
+     *   9   5       16
+     */
 
+     // Replace the below line to create a tree 
+     // as represented in the diagram above
+     Node<Integer> smallTree = null;
+
+
+    /*
+     *              42
+     *          /       \
+     *       17           63
+     *      /  \            \
+     *     9    21           87
+     *    /    /  \          /
+     *   3    19   25     76
+    */
+
+    Node<Integer> largeTree = new Node<>(42,
+        new Node<>(17, // 17 is left child of 42
+                new Node<>(9, // 9 is the left child of 17
+                        new Node<>(3, null, null), // 3 is the left child of 9
+                        null), // no right child of 9
+                new Node<>(21, // 21 is the right child of 17
+                        new Node<>(19, null, null), // 19 is the left child of 21
+                        new Node<>(25, null, null))), // 25 is the right child of 21
+        new Node<>(63, // 63 is right child of 42
+                null, // no left child of 63
+                new Node<>(87, // 87 is right child of 63
+                        new Node<>(76, null, null), // 76 is the left child of 87
+                        null))); // no right child of 87
   }
 }
